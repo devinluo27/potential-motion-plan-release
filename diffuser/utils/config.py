4353e7b1828ca8aa1403d2c3d65507ff736014d8
devinluo27 <945517787@qq.com>
2024-07-06 17:06:21 -0400

    [update] for colab compatibility.

--- diffuser/utils/config.py
+++ diffuser/utils/config.py
@@ -19,7 +19,7 @@ def import_class(_class):
     print(f'[ utils/config ] Imported {repo_name}.{module_name}:{class_name}')
     return _class
 
-class Config(collections.Mapping):
+class Config(collections.abc.Mapping):
 
     def __init__(self, _class, verbose=True, savepath=None, device=None, **kwargs):
         self._class = import_class(_class)
--- pb_diff_envs/pb_diff_envs/utils/graphs.py
+++ pb_diff_envs/pb_diff_envs/utils/graphs.py
@@ -1,12 +1,15 @@
 import numpy as np
 import torch
-from torch_geometric.nn import knn_graph
-from torch_sparse import coalesce
+
 from .utils import create_dot_dict
 from collections import defaultdict
 
 
 def knn_graph_from_points(points, k):
+    ## move to inside the func so the code can run without installing these packages
+    from torch_geometric.nn import knn_graph
+    from torch_sparse import coalesce
+
     edge_index = knn_graph(torch.FloatTensor(np.array(points)), k=k, loop=True)
     edge_index = torch.cat((edge_index, edge_index.flip(0)), dim=-1)
     edge_index_torch, _ = coalesce(edge_index, None, len(points), len(points))
